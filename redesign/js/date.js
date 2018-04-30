a8f0e1afb7c15eb0b6c46f62571660e8e08d9630
Xinchi <38084090+Swilor@users.noreply.github.com>
2018-04-30 01:00:56 -0700

    finally fixed the live status

--- redesign/js/date.js
+++ redesign/js/date.js
@@ -7,9 +7,10 @@ var d = new Date(); //"=" assign opreator
 
 
 
-//change default 24 hour time to 12 hour time
-var hour = (d.getHours());
 
+var hour = d.getHours();
+
+//change default 24 hour time to 12 hour time
 //   if (hour === 0){
 //        hour = 12;
 //     }else if (hour >= 13){
@@ -24,31 +25,26 @@ var n = day[d.getDay()];
 var min = d.getMinutes();
 
 // judge the status
-//var s = 1; another way to write these codes
+var s = 1;
 
-if (n === day[6]){
+if (n === day[0]){
   var s = 1;
 }
   else if(hour>=12 && hours <=15){
       var s = 0;
     }
-  else if(hour>=17 && hours <=20){
+  else if(hour>=17 && hours <20){
       var s = 0;
     }
-
-  else if(hours=11 && min>=30 && min<=59 ){
+  else if(hour=11 && min>=30 && min<=59 ){
       var s = 0;
     }
-  else if(hours=16 && min>=30 && min<=59){
+  else if(hour=16 && min>=30 && min<=59){
       var s = 0;
     }
-  else if(hours=20 && min>=30 && min<=59){
+  else if(hour=20 && min>=30 && min<=59){
       var s = 0;
     }
-    else {         //delete this line if you want the other way to write it.
-      var s = 1;  //delete this line if you want the other way to write it.
-    }             //delete this line if you want the other way to write it.
-
 
 
 document.getElementById("today").innerHTML = "Hours" + ":" + "" + status[s];
